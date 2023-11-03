192c05a398af596e86fdb8dd2123a35c53ecbfce
cdx <77438859@qq.com>
2023-11-03 16:48:46 +0800

    修改打包exe的逻辑

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ import os
 import datetime
 from send_email import send_email
 
-def get_resource_path(relative_path):
-    """ 返回资源的绝对路径，用于访问包含在 PyInstaller --onefile 中的资源 """
-    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(base_path, relative_path)
-
 def read_config(config_filename='config.txt'):
-    config_path = get_resource_path(config_filename)
+    config_path = os.path.join(os.getcwd(), config_filename)  # 直接使用当前目录路径
     config = {}
     try:
         with open(config_path, 'r', encoding='utf-8') as f:
@@ -33,8 +28,7 @@ def find_last_connection_attempt(log_content):
 def main():
     config = read_config()
 
-    # 从当前工作目录获取路径
-    log_path = os.path.join(os.getcwd(), 'debug', 'gui.log')
+    log_path = os.path.join(os.getcwd(), 'debug', 'gui.log')  # 直接使用当前目录路径
 
     try:
         with open(log_path, 'r', encoding='utf-8') as log_file:
@@ -44,18 +38,18 @@ def main():
         sys.exit(1)
     
     last_connection_line, log_to_send = find_last_connection_attempt(log_content)
-    print( {log_to_send} )
+    print({log_to_send})
     if last_connection_line is None:
         print("未在日志中找到连接尝试。")
         sys.exit(1)
     
-    current_time = datetime.datetime.now().strftime("%m-%d %H:%M")
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     if "任务出错" in log_to_send:
         subject = f"{current_time} 运行出错-MAA运行日志"
-        content = f"{current_time}运行出错！请检查出错情况。\n\n{log_to_send}"
+        content = f"{current_time} 运行出错！请检查出错情况。\n\n{log_to_send}"
     else:
         subject = f"{current_time} 运行成功-MAA运行日志"
-        content = f"{current_time}运行成功！\n\n{log_to_send}"
+        content = f"{current_time} 运行成功！\n\n{log_to_send}"
     
     send_email(
         smtp_server=config['smtp_server'],
@@ -69,6 +63,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
